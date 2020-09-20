be7911076237bdcc2e0a99759e2169e7539e1381
P.F.C <firefirstfu@gmail.com>
2020-09-20 19:34:01 +0800

    ok:錯誤攔截

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import router from './router'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import store from './store'
 import App from './App.vue'
 
+// 根據前端的跨域方式做調整 /a/b : /api/a/b => /a/b
+axios.defaults.baseURL = '/api'
+axios.defaults.timeout = 8000
+// 錯誤攔截
+axios.interceptors.response.use(function (response) {
+  const res = response.data
+  if (res.status === 0) {
+    return res.data
+  } else if (res.status === 10) {
+    window.location.href = '/#/login'
+  } else {
+    alert(res.msg)
+  }
+})
+
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
