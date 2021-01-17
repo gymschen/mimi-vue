a7f82168f46b679f475e4326c5ca047915589b48
firefirstfu <firefirstfu@gmail.com>
2021-01-18 05:21:02 +0800

    commit

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <h1>index</h1>
+    <div class="father">
+      <div class="block">1</div>
+      <div class="block">2</div>
+      <div class="block">3</div>
+      <div class="block">4</div>
+      <div class="block">5</div>
+    </div>
   </div>
 </template>
 
@@ -10,3 +16,17 @@ export default {
   name: 'index'
 }
 </script>
+
+<style lang="scss" >
+.father {
+  display: flex;
+  flex-direction: row;
+}
+.block {
+  background-color: #ccc;
+  margin: 10px;
+  // display: inline-block;
+  width: 50px;
+  height: 50px;
+}
+</style>
