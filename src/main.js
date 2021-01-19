45c1f05f7a71711933fcd451ec265af777373451
firefirstfu <firefirstfu@gmail.com>
2021-01-20 00:07:34 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import router from './router'
-
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-
 import store from './store'
 import App from './App.vue'
+import env from './env'
+
+console.log('env:', env)
 
 // 根據前端的跨域方式做調整 /a/b : /api/a/b => /a/b
-axios.defaults.baseURL = '/api'
+axios.defaults.baseURL = env.baseURL
 axios.defaults.timeout = 8000
 // 錯誤攔截
 axios.interceptors.response.use(function (response) {
