2e9eae4d43a376fe311845c0a77919da664b2091
firefirstfu <firefirstfu@gmail.com>
2021-01-20 01:31:22 +0800

    commit

---
+++ src/components/ServiceBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="service">
+    <div class="container">kkk</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'service-bar'
+}
+</script>
+
+<style lang="scss">
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <ServiceBar></ServiceBar>
+  </div>
 </template>
 
 <script>
+import ServiceBar from '@/components/ServiceBar'
 
 export default {
-  name: 'index'
+  name: 'index',
+  components: {
+    ServiceBar
+  }
 }
 </script>
 
